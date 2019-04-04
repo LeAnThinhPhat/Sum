92762bbe90196f81f285520a0c58747f927b2344
LeAnThinhPhat <48433100+LeAnThinhPhat@users.noreply.github.com>
2019-04-04 22:46:38 +0700

    Add files via upload

---
+++ Sum1.js
@@ -0,0 +1,27 @@
+var fs = require('fs');
+function sum(a, b, cb){
+    var result = 0;
+    setTimeout(()=>{
+        result = a + b;
+        console.log("timeout excuted!");
+        cb(result);
+    },3000)
+     
+    return result;
+};
+
+function main(){
+    var res = sum(1,2,(data)=>{
+        console.log(data);
+        
+            fs.writeFile('./ketqua1.txt', data, (err)=>{
+                if(err) throw err;                  
+                console.log('Finish'); 
+            });
+    });
+    console.log('Start');
+    
+
+};
+
+main();
